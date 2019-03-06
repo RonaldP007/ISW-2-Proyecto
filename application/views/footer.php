b878e402f6b52dd9bad970e7390174cadf73dd8a
Kendall Rentana <kendallandreyr@gmail.com>
2019-03-05 21:18:25 -0600

    Se agregaron los script ajax para poder trabajar con los modals

--- application/views/footer.php
+++ application/views/footer.php
@@ -5,6 +5,14 @@
 
         </div>
     </div>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Remember to include jQuery :)--->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+    <!-- jQuery Modal -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
+    <!--<script type="text/javascript" src="<?php //echo base_url();?>assets/js/verificarAdmin.js"></script>-->
 </body>
 
 </html>
