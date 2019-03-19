cb933ff83a6d963951c34ac28b914ab94066d6c5
Kendall Rentana <kendallandreyr@gmail.com>
2019-03-18 21:51:46 -0600

    se cambio el valor de estado_factura a "p" ya que booleano no sirvio. Y se cambio el return de la informacion en la funcion estadoCredito.

--- application/models/Fact_Credito_Model.php
+++ application/models/Fact_Credito_Model.php
@@ -3,49 +3,44 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 Class Fact_Credito_Model extends CI_model{
 
-    public $id_usuario_credi;
-    public $id_cliente_credito;
+  public $id_usuario_credi;
+  public $id_cliente_credito;
 	public $fecha_credito;
 	public $nombre_produc;
 	public $precios_produc;
-    public $cantidades_produc;  
-    public $total_factura;
-    public $total_creditar;
-    public $total_adeudado;
-    public $estado_factura;
+  public $cantidades_produc;  
+  public $total_factura;
+  public $total_creditar;
+  public $total_adeudado;
+  public $estado_factura;
 
-    public function __construct(){
-		$this->load->database();
-    }
-    
-    public function registroCredito($id_usuario_credi, $id_cliente_credito, $fecha_credito, $nombre_produc, $precios_produc, $cantidades_produc, $total_factura, $total_creditar, $total_adeudado){
-        $this->id_usuario_credi = $id_usuario_credi;
-        $this->id_cliente_credito = $id_cliente_credito; 
-        $this->fecha_credito = $fecha_credito;
-        $this->nombre_produc = $nombre_produc;
-        $this->precios_produc = $precios_produc;
-        $this->cantidades_produc = $cantidades_produc;
-        $this->total_factura = $total_factura;
-        $this->total_creditar = $total_creditar;
-        $this->total_adeudado = $total_adeudado;
-        $this->estado_factura = false;
-        
-        return $this->db->insert('factura_credito', $this);
-    }
+  public function __construct(){
+  $this->load->database();
+  }
+  
+  //registra una nueva factura credito.
+  public function registroCredito($id_usuario_credi, $id_cliente_credito, $fecha_credito, $nombre_produc, $precios_produc, $cantidades_produc, $total_factura, $total_creditar, $total_adeudado){
+      $this->id_usuario_credi = $id_usuario_credi;
+      $this->id_cliente_credito = $id_cliente_credito; 
+      $this->fecha_credito = $fecha_credito;
+      $this->nombre_produc = $nombre_produc;
+      $this->precios_produc = $precios_produc;
+      $this->cantidades_produc = $cantidades_produc;
+      $this->total_factura = $total_factura;
+      $this->total_creditar = $total_creditar;
+      $this->total_adeudado = $total_adeudado;
+      $this->estado_factura = "p";
+      
+      return $this->db->insert('factura_credito', $this);
+  }
 
-    public function estadoCredito($usuario){
-        $this->db->select('*');
-        $this->db->from('factura_credito'); 
-        $this->db->where('id_cliente_credito',$usuario);
-        $query=$this->db->get();
-    
-        if($query->num_rows()>0){
-          $valor = 1;
-          return $valor;
-    
-        }else{
-          $valor = 0;
-          return $valor;
-        }
-    }
+  //extrae la informacion de facturas credito de acuerdo el cliente. 
+  public function estadoCredito($usuario){ 
+      $this->db->select('*');
+      $this->db->from('factura_credito'); 
+      $this->db->where('id_cliente_credito', $usuario); //
+      $query= $this->db->get();
+
+      return $query->result_array();
+  }
 }
