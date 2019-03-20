d4de03269f414e710a16c074600b32ce960ab107
Kendall Rentana <kendallandreyr@gmail.com>
2019-03-20 17:38:52 -0600

    Se dio estilos a el panel heading y se cambio clase a boton editar.

--- application/views/Productos/Productos_View.php
+++ application/views/Productos/Productos_View.php
@@ -7,7 +7,8 @@
 <html lang="en">
   <head>
     <title>Productos</title>
-	  <link href="<?php echo base_url(); ?>/assets/css/bootstrap.css" rel="stylesheet">
+	  <!--<link href="<?php //echo base_url(); ?>/assets/css/bootstrap.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	  <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/styles.css">
   </head>
 
@@ -25,9 +26,9 @@
 
       <div class="panel panel-info" style="margin-top: 20px;">
         
-        <div class="panel-heading" style="display: flex; text-align: center;">
-            <h3 class="panel-title" style="margin-top: 5px;">Productos</h3>
-            <a style="margin-left: 980px;" class="new btn btn-sm btn-primary" href="<?= base_url('Productos/registro') ?>">Agregar</a>
+        <div class="panel-heading" style="display: flex; text-align: center; background-color: #d9edf7;">
+            <h3 class="panel-title" style="margin-top: 5px; font-size: 150%; color: #31708f;">Productos</h3>
+            <a style="margin-top: 0.20%; margin-left: 82.68%; height: 1%;" class="btn btn-primary" href="<?= base_url('Productos/registro') ?>">Agregar</a>
         </div>
 
         <div class="panel-body detalle-producto">
@@ -52,39 +53,39 @@
 
                   <?php foreach($productos as $item){?>
               		<tr>
-						<?php 
-						$id = $item['id'];           
-						?>
+                  <?php 
+                  $id = $item['id'];           
+                  ?>
 
-						<td><?php  
-							echo $item['id'];
-						?></td>
+                  <td><?php  
+                    echo $item['id'];
+                  ?></td>
 
-						<td><?php 
-						echo $item['nombre'];
-						?></td>
+                  <td><?php 
+                  echo $item['nombre'];
+                  ?></td>
 
-						<td><?php 
-							echo $item['cantidad'];
-						?></td>
+                  <td><?php 
+                    echo $item['cantidad'];
+                  ?></td>
 
-						<td><?php 	
-							echo $item['precio']; 
-						?></td>
+                  <td><?php 	
+                    echo $item['precio']; 
+                  ?></td>
 
-						<td><?php 
-							echo $item['nombre_pv'];
-						?></td>
+                  <td><?php 
+                    echo $item['nombre_pv'];
+                  ?></td>
 
-						<td><a class="btn btn-sm btn-info" href="<?php echo base_url() . "Productos/Producto/" . $id?>">Editar</a></td>
-						<td><a class="btn btn-sm btn-danger" href="<?php echo base_url() . "Productos/eliminar/" . $id?>">Eliminar</a></td>
+                  <td><a class="btn btn-info" href="<?php echo base_url() . "Productos/Producto/" . $id?>">Editar</a></td>
+                  <td><a class="btn btn-sm btn-danger" href="<?php echo base_url() . "Productos/eliminar/" . $id?>">Eliminar</a></td>
 
                   </tr>
-                          
-                  <?php }?>
-                    
-                </tr>
-              </tbody>
+                            
+                    <?php }?>
+                      
+                  </tr>
+                </tbody>
               
             </table>
 
