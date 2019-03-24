d7df8a216be4ae47d7e191dbde9689824f37865a
Kendall Rentana <kendallandreyr@gmail.com>
2019-03-23 23:18:15 -0600

    se agrego el link para jQuery

--- application/views/header.php
+++ application/views/header.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" media="screen" title="no title">
     <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/styles.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/index.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
 </head>
 
